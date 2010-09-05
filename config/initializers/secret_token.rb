a066f652f654f7d211f7e058417affcc62e4b53c
Martin Stig Stissing <martinstissing@gmail.com>
2010-09-05 03:47:18 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubadub::Application.config.secret_token = 'f5c5819eb61df38908a2a455a4859f4c49d1c2240c8b6ccc92869ddb06631018b404865e8d586516c6c8510b812c192fe2edf87946acae25f5dfdafd1ab11e50'
